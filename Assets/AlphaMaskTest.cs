319df5d195fd7c50a1156e7868b81c7c42a51885
kangms5296 <rlfhfhdhk@naver.com>
2019-05-17 23:06:34 +0900

    장소 이동간 사용할 Alpha Mask Asset 추가

---
+++ Assets/AlphaMaskTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class AlphaMaskTest : MonoBehaviour {
+
+    public RectTransform t;
+
+    [Range(0.001f, 30)]
+    public float size = 30;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        t.localScale = new Vector3(size, size, 1);
+	}
+}
